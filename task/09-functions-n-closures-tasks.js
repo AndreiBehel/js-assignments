649be45c888ef260f2047870161019d90c699094
AndreiBehel <andrew.kobrin@gmail.com>
2017-02-26 13:02:28 +0300

    functions-n-closures: add solution for the task 1-9

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -26,7 +26,9 @@
  *
  */
 function getComposition(f,g) {
-    throw new Error('Not implemented');
+    return function (x) {
+        return f(g(x));
+    }
 }
 
 
@@ -47,7 +49,9 @@ function getComposition(f,g) {
  *
  */
 function getPowerFunction(exponent) {
-    throw new Error('Not implemented');
+    return function (value) {
+        return Math.pow(value, exponent);
+    };
 }
 
 
@@ -65,7 +69,16 @@ function getPowerFunction(exponent) {
  *   getPolynom()      => null
  */
 function getPolynom() {
-    throw new Error('Not implemented');
+    if (arguments.length == 0) {
+        return null;
+    }
+    //convert to array and reverse the array
+    var args = Array.from(arguments).reverse();
+    return function (x) {
+        return args.reduce(function (accum, val, index) {
+            return accum += val * Math.pow(x, index);
+        });
+    }
 }
 
 
@@ -84,7 +97,10 @@ function getPolynom() {
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
 function memoize(func) {
-    throw new Error('Not implemented');
+    var cache = func();
+    return function () {
+        return cache;
+    };
 }
 
 
@@ -104,7 +120,16 @@ function memoize(func) {
  * retryer() => 2
  */
 function retry(func, attempts) {
-    throw new Error('Not implemented');
+    return function () {
+        var i = 0;
+        while (i++ < attempts) {
+            try {
+                return func();
+            } catch (e) {
+
+            }
+        }
+    }
 }
 
 
@@ -132,7 +157,18 @@ function retry(func, attempts) {
  *
  */
 function logger(func, logFunc) {
-    throw new Error('Not implemented');
+    return function () {
+        var str = func.name + '(';
+        var arrayArg = Array.from(arguments);
+        //conver to string representation
+        var json = JSON.stringify(arrayArg);
+        //delete square brackets from beginning and end of the string
+        str += json.slice(1, -1) + ')';
+        logFunc(str + ' starts');
+        var result = func.apply(null, arrayArg);
+        logFunc(str + ' ends');
+        return result;
+    };
 }
 
 
@@ -150,7 +186,13 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(fn) {
-    throw new Error('Not implemented');
+    //conver args to array
+    var args = Array.from(arguments);
+    return function () {
+        //convert arguments and concat to the previous array
+        args = args.concat(Array.from(arguments));
+        return args[0](args[1], args[2], args[3], args[4]);
+    };
 }
 
 
@@ -171,7 +213,11 @@ function partialUsingArguments(fn) {
  *   getId10() => 11
  */
 function getIdGeneratorFunction(startFrom) {
-    throw new Error('Not implemented');
+    var currentCount = startFrom;
+
+    return function() {
+        return currentCount++;
+    };
 }
 
 

6bd40f6e06b2f6a084bb0a1c690d225c4081576e
AndreiBehel <andrew.kobrin@gmail.com>
2017-03-05 22:45:48 +0300

    katas-3: add fix #2 for permute task

--- task/12-katas-3-tasks.js
+++ task/12-katas-3-tasks.js
@@ -53,7 +53,9 @@ function* getPermutations(chars) {
             for (let i = 0; i < n; i++) {
                 yield *permute(a, n - 1);
                 const j = n % 2 ? 0 : i;
-                [a[n-1], a[j]] = [a[j], a[n-1]];
+                let temp = a[n - 1];
+                a[n - 1] = a[j];
+                a[j] = temp;
             }
     }
     yield *permute(chars.split(''), chars.length);
